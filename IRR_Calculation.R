41bfded1f2e332a2d7a720c19ae34db699adaca0
Real Limoges <b.real.limoges@gmail.com>
2015-04-27 18:54:19 -0400

    Add Mode

--- IRR_Calculation.R
+++ IRR_Calculation.R
@@ -3,18 +3,33 @@
 # is 0. To do this, I numerically minimize the square of the NPV 
 # function, so until I hit a minima
 
+year_len <- 365.25
+initial_IRR <- 0.2
+max_step <- 0.1
+
 NPV <- function(payments, dates, IRR){
 	time_diff <- dates - min(dates)
-	time_diff <- as.numeric(dates, units = "days")
+	time_diff <- as.numeric(dates, units = "days") / year_len
 	NPV <- sum(payments * (1 + IRR)^{-dates})
 
 	return(NPV)
 }
 
+# Finds the IRR iteratively; a maximum step is imposed on the gradient
+# to prevent the solution from straying too far from 1.0
 find_IRR <- function(){
-	# Initial conditions are chosen since funds typically have
-	# IRRs of 20%
-	IRR <- nlm( function(p){ NPV(payments, dates, p) ^ 2}, p = 0.2, stepmax = 0.1) )
+	IRR <- nlm(function(p){ NPV(payments, dates, p) ^ 2}, 
+		       p = initial_IRR, stepmax = max_step) )
+	
 	return(IRR)
 }
 
+# Finds the mode and 
+Mode <- function(x) {
+	unique_x <- unique(x)
+	Mode_num <- [which.max(tabulate(match(x, unique_x)))]
+	Mode_count <- max(tabulate(match(x, unique_x))) 
+	Mode <- c(Mode_num, Mode_count)
+	
+	return(Mode)
+}
